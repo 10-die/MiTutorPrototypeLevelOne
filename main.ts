e05da8cb419de2c9f12c74172b083cfc7684aa03
10die <161975861+10-die@users.noreply.github.com>
2024-07-17 22:51:18 +0200

    buttons with cross html fucntionality

---
+++ main.ts
@@ -0,0 +1,3 @@
+document.getElementById('btnSignIn')?.addEventListener('click', () => {window.location.href = 'home.html';});
+
+document.getElementById('btnSingOut')?.addEventListener('click', () => {window.location.href = 'signIn.html';});
